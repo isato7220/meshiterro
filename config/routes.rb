a2c9a9a36571072830223b52ed614b5cac712a83
Ubuntu <ubuntu@ip-172-31-28-172.ap-northeast-1.compute.internal>
2025-12-09 01:28:59 +0000

    [admin]

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resources :post_comments, only: [:create, :destroy]
     resource :favorite, only: [:create, :destroy]
